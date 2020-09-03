954c4ac9a7a5a5b29d575ba714838e0cdc9ad94e
simen <simen@192.168.0.20>
2020-09-03 13:57:46 +0200

    update

---
+++ Importsetninger/src/samling/Importsetninger.java
@@ -0,0 +1,19 @@
+package samling;
+
+import static java.lang.Math.*; //abs, max, min, round, sqrt, pow(potens)
+import easygraphics.*;
+import static java.lang.System.*; // for utskrift til konsoll
+import static javax.swing.JOptionPane.*; //for kommunikasjon via dialogvindu
+import static java.lang.Integer.*;
+import static java.lang.Double.*;
+//import static org.junit.Assert.*; for å teste at utfall blir som forventet
+//import org.junit.Test;
+
+public class Importsetninger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Importsetninger/src/samling/Kommandoer.java
@@ -0,0 +1,48 @@
+package samling;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static javax.swing.JOptionPane.showInputDialog;
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class Kommandoer {
+
+	public static void main(String[] args) {
+
+		System.out.println();// printer ut på konsoll
+
+		// String tallTxt = showInputDialog("Gi et tall:");
+		// int tall = parseInt(tallTxt);
+		// String utTxt = "Svar: " + dobbel;
+		// showMessageDialog(null, utTxt);
+		byte x;
+		x = 4;
+
+		short y;
+		y = 0;
+
+		int z;
+		x = -7;
+
+		long w;
+		w = 31;
+
+		double v;
+		v = 3.14;
+
+		float f;
+		f = 2.456f;
+
+		char c;
+		c = 'a';
+
+		boolean b;
+		b = true;
+		b = false;
+
+		String str;
+		str = "DAT100";
+
+	}
+
+}
